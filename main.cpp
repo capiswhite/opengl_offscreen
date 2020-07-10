1dc17cb5b094fed9e665bd4bd84210e14492321d
CYP <1356261895@qq.com>
2020-07-10 15:54:26 +0800

    egl_offscreen pbuffer

--- main.cpp
+++ main.cpp
@@ -27,125 +27,125 @@ void Rectangle();
 int main(int argc, char *argv[])
 {
 
-    // EGLDisplay display;
-	// EGLConfig config;
-	// EGLContext context;
-	// EGLSurface surface;
-	// //NativeWindowType native_window;
-	// EGLint num_config;
+    EGLDisplay display;
+	EGLConfig config;
+	EGLContext context;
+	EGLSurface surface;
+	//NativeWindowType native_window;
+	EGLint num_config;
 
-	// const EGLint attribute_list[] = {
-	// 	EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
-	// 	EGL_BLUE_SIZE, 8,
-	// 	EGL_GREEN_SIZE, 8,
-	// 	EGL_RED_SIZE, 8,
-	// 	EGL_DEPTH_SIZE, 8,
-	// 	EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
-	// 	EGL_NONE
-	// };
-	// const int pbufferWidth = Width;
-	// const int pbufferHeight = Height;
-	// const EGLint pbufferAttribs[] = {
-	// 	  EGL_WIDTH, pbufferWidth,
-	// 	  EGL_HEIGHT, pbufferHeight,
-	// 	  EGL_NONE
-	// };
-
-
-	// //* get an EGL display connection */
-    // display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-	// if (display == EGL_NO_DISPLAY)
-	// {
-	// 	cout << "Get display fail." << endl;
-	// 	return 0;
-	// }
-	// cout << display << endl;
-
-	// /* initialize the EGL display connection */
-	// EGLint major = 0;
-	// EGLint minor = 1;
-
-	// if (eglInitialize(display, &major, &minor))
-	// {
-	// 	cout << "ELG" << (EGLint)major << "." << (EGLint)minor << endl;
-	// }
-	// else
-	// {
-	// 	EGLint  err = eglGetError();
-
-	// 	cout << "eglInitialize fail." << endl;
-	// 	cout << err << endl;
-	// }
+	const EGLint attribute_list[] = {
+		EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+		EGL_BLUE_SIZE, 4,
+		EGL_GREEN_SIZE, 4,
+		EGL_RED_SIZE, 4,
+		EGL_DEPTH_SIZE, 4,
+		EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+		EGL_NONE
+	};
+	const int pbufferWidth = Width;
+	const int pbufferHeight = Height;
+	const EGLint pbufferAttribs[] = {
+		  EGL_WIDTH, pbufferWidth,
+		  EGL_HEIGHT, pbufferHeight,
+		  EGL_NONE
+	};
 
 
-	// /* get an appropriate EGL frame buffer configuration */
-	// if (!eglChooseConfig(display, attribute_list, &config, 1, &num_config))
-	// {
-	// 	cout << "eglChooseConfig fail." << endl;
-	// }
-	// else
-	// {
+	//* get an EGL display connection */
+    display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+	if (display == EGL_NO_DISPLAY)
+	{
+		cout << "Get display fail." << endl;
+		return 0;
+	}
+	cout << display << endl;
 
-	// }
+	/* initialize the EGL display connection */
+	EGLint major = 0;
+	EGLint minor = 1;
 
-	// //EGLint attribute_list_2[] = {
-	// //EGL_CONTEXT_CLIENT_VERSION, 3,
-	// //EGL_NONE
-	// //};
+	if (eglInitialize(display, &major, &minor))
+	{
+		cout << "ELG" << (EGLint)major << "." << (EGLint)minor << endl;
+	}
+	else
+	{
+		EGLint  err = eglGetError();
 
-	// ///* create an EGL rendering context */
-	// context = eglCreateContext(display, config, 0 , NULL);
-
-	// if (context == 0)
-	// {
-	// 	cout << "Creat Contest fail." << endl;
-	// 	//return false;
-	// }
-	// else
-	// {
-	// 	cout << "Susscess Creat Contest" << endl;
-	// }
+		cout << "eglInitialize fail." << endl;
+		cout << err << endl;
+	}
 
 
+	/* get an appropriate EGL frame buffer configuration */
+	if (!eglChooseConfig(display, attribute_list, &config, 1, &num_config))
+	{
+		cout << "eglChooseConfig fail." << endl;
+	}
+	else
+	{
 
-	// /* create a surface */
-	// //native_window = createNativeWindow();
-	// eglBindAPI(EGL_OPENGL_API);
+	}
+
+	//EGLint attribute_list_2[] = {
+	//EGL_CONTEXT_CLIENT_VERSION, 3,
+	//EGL_NONE
+	//};
+
+	///* create an EGL rendering context */
+	context = eglCreateContext(display, config, 0 , NULL);
+
+	if (context == 0)
+	{
+		cout << "Creat Contest fail." << endl;
+		//return false;
+	}
+	else
+	{
+		cout << "Susscess Creat Contest" << endl;
+	}
+
+
+
+	/* create a surface */
+	//native_window = createNativeWindow();
+	eglBindAPI(EGL_OPENGL_API);
 
 	
-    // //EGLNativePixmapType pixmap;
-    // //surface = eglCreatePixmapSurface(display, config, pbufferAttribs);
+    //EGLNativePixmapType pixmap;
+    //surface = eglCreatePixmapSurface(display, config, pbufferAttribs);
 
-    // /* create an EGL Pbuff surface */
-    // surface = eglCreatePbufferSurface (display, config, pbufferAttribs);
+    /* create an EGL Pbuff surface */
+    surface = eglCreatePbufferSurface (display, config, pbufferAttribs);
 
 
-	// EGLint w,h;
-	// if(!eglQuerySurface(display ,surface,EGL_WIDTH,&w)||!eglQuerySurface(display ,surface,EGL_HEIGHT,&h))
-	// {
-	// 	cout << "Creat surface fail." << endl;
-	// 	//return false;
-	// }
-	// else 
-	// {
-	// 	cout << "Susscess Creat surface." << surface << endl;
-	// }
+	EGLint w,h;
+	if(!eglQuerySurface(display ,surface,EGL_WIDTH,&w)||!eglQuerySurface(display ,surface,EGL_HEIGHT,&h))
+	{
+		cout << "Creat surface fail." << endl;
+		//return false;
+	}
+	else 
+	{
+		cout << "Susscess Creat surface." << surface << endl;
+	}
 
 
 	/* connect the context to the surface */
 
-	//if (!eglMakeCurrent(display, surface, surface, context))
-	//{
-	//	cout << "Connect fail." << endl;
-	//	return 0;
-	//} 
+	if (!eglMakeCurrent(display, surface, surface, context))
+	{
+		cout << "Connect fail." << endl;
+		return 0;
+	} 
 
-    glutInit(&argc, argv);
-    glutInitWindowSize(1, 1);
-    glutInitDisplayMode(GLUT_RGBA);
+    //glutInit(&argc, argv);
+    //glutInitWindowSize(1, 1);
+    //glutInitDisplayMode(GLUT_RGBA);
 
-    int window  = glutCreateWindow("project of openGL");
-    glutDestroyWindow(window);
+    //int window  = glutCreateWindow("project of openGL");
+    //glutDestroyWindow(window);
     //glGetIntegerv(GL_AUX_BUFFERES, IntV);
 
     //glutDisplayFunc(Rectangle);
@@ -155,60 +155,68 @@ int main(int argc, char *argv[])
 
     //cout << "Susscess GLUT." << endl;
 
-    glewInit();
+    //glewInit();
 
-    unsigned int m_tex = 0;
-    unsigned int m_FboID = 0;
-	unsigned int m_RboID = 0;
+    //unsigned int m_tex = 0;
+    //unsigned int m_FboID = 0;
+	//unsigned int m_RboID = 0;
     
+	// glGenTextures(1,&m_tex);
+	// glBindTexture(GL_TEXTURE_2D,m_tex);
+    // glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,Width,Height,0,GL_RGBA,GL_UNSIGNED_BYTE,NULL); 
+    // glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); 
+    // glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); 
 
-	glGenTextures(1,&m_tex);
-	glBindTexture(GL_TEXTURE_2D,m_tex);
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,Width,Height,0,GL_RGBA,GL_UNSIGNED_BYTE,NULL); 
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); 
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); 
+	// glGenRenderbuffers(1,&m_RboID);
+	// glBindRenderbuffer(GL_RENDERBUFFER,m_RboID);
+	// glRenderbufferStorageEXT(GL_RENDERBUFFER,GL_DEPTH_COMPONENT, Width, Height);
+	// glBindRenderbufferEXT(GL_RENDERBUFFER,0);
 
-	glGenRenderbuffers(1,&m_RboID);
-	glBindRenderbuffer(GL_RENDERBUFFER,m_RboID);
-	glRenderbufferStorageEXT(GL_RENDERBUFFER,GL_DEPTH_COMPONENT, Width, Height);
-	glBindRenderbufferEXT(GL_RENDERBUFFER,0);
-
-	glGenFramebuffersEXT(1,&m_FboID);
-	glBindFramebufferEXT(GL_FRAMEBUFFER,m_FboID);
-	glFramebufferTexture2DEXT(GL_FRAMEBUFFER,GL_COLOR_ATTACHMENT0,GL_TEXTURE_2D,m_tex,0);
-	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER,GL_DEPTH_ATTACHMENT,GL_RENDERBUFFER,m_RboID);
-	GLenum status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER);
-    glBindFramebuffer(GL_FRAMEBUFFER,0);
+	// glGenFramebuffersEXT(1,&m_FboID);
+	// glBindFramebufferEXT(GL_FRAMEBUFFER,m_FboID);
+	// glFramebufferTexture2DEXT(GL_FRAMEBUFFER,GL_COLOR_ATTACHMENT0,GL_TEXTURE_2D,m_tex,0);
+	// glFramebufferRenderbufferEXT(GL_FRAMEBUFFER,GL_DEPTH_ATTACHMENT,GL_RENDERBUFFER,m_RboID);
+	// GLenum status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER);
+    // glBindFramebuffer(GL_FRAMEBUFFER,0);
 
     glClearColor(1.0f, 0.0f, 1.0f , 1.0f);
     glClearDepth(1.0);
 
     
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER,m_FboID);
+    // glBindFramebuffer(GL_DRAW_FRAMEBUFFER,m_FboID);
     glViewport(0, 0, Width, Height);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();  
     //gluOrtho2D(0, 30,0, 20);
-    glColor3f(0.0f, 0.0f, 0.7f);
-    //glLineWidth(10);
-    glDrawBuffer(GL_FRONT);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(0.10f, 0.0f);
-    glVertex2f(0.5f, 0.10f);
-    glVertex2f(0.40f, 0.40f);
-    glEnd();
-    glFlush();
     
-    glColor3f(0.7f, 0.0f, 0.0f);
-    glDrawBuffer(GL_BACK);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(0.20f, 0.10f);
-    glVertex2f(0.25f, 0.10f);
-    glVertex2f(0.60f, 0.50f);
-    glEnd();
+    glColor4f(1.0f, 1.0f, 1.0f , 1.0f);
+    //glLineWidth(10);
+    //glDrawBuffer(GL_FRONT);
+    GLfloat  vertices_tri[] ={0.10f, 0.0f , 0.2f,
+                              0.5f, 0.10f , 0.3f,
+                            0.40f, 0.40f, 0.4f };
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, vertices_tri);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
     glFlush();
+    glDisableClientState(GL_VERTEX_ARRAY);
+    //glBegin(GL_TRIANGLES);
+    //glVertex3f(0.10f, 0.0f , 0.2f );
+    //glVertex3f(0.5f, 0.10f , 0.3f ) ;
+    //glVertex3f(0.40f, 0.40f, 0.4f );
+    //glEnd();
+    //glFlush();
+    
+    // glColor4f(0.0f, 0.0f, 0.0f , 0.5f);
+    // //glDrawBuffer(GL_BACK);
+    // glBegin(GL_TRIANGLES);
+    // glVertex2f(0.20f, 0.10f);
+    // glVertex2f(0.25f, 0.10f);
+    // glVertex2f(0.60f, 0.50f);
+    // glEnd();
+    // glFlush();
     //glutSwapBuffers();
     
 
@@ -222,7 +230,7 @@ int main(int argc, char *argv[])
     //glFlush();
     //eglSwapBuffers(display, surface);
 
-    glColor3f(0.7f, 0.5f, 0.5f);
+    glColor4f(1.0f, 1.0f, 0.0f , 1.0f);
     GLfloat vertices[] =   {0.0f, 0.0f, 0.0f,
                             0.0f, 0.2f, 0.0f,
                             0.0f, 0.2f, 0.2f,
@@ -239,7 +247,7 @@ int main(int argc, char *argv[])
                          5,2,1, 5,2,6,
                          7,3,2, 7,6,2};
     glLoadIdentity();
-    gluLookAt( 0.5, 0.5 ,  0.5 , 0.2, 0.2, 0.2,  0, 1, 0);
+    gluLookAt( 0.5, 0.5 ,  0.5 , 0.2, 0.3, 0.4 ,  0, 1, 0);
     // 激活顶点数组
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(3, GL_FLOAT, 0, vertices);
@@ -251,12 +259,16 @@ int main(int argc, char *argv[])
     glDisableClientState(GL_VERTEX_ARRAY);
     glFlush();
 
-    //eglDestroySurface(display, surface);
-    //eglDestroyContext(display, context);
-    //eglTerminate(display);
 
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER,0);
-    grab(m_FboID);
+
+    //glBindFramebuffer(GL_DRAW_FRAMEBUFFER,0);
+    grab(0);
+
+
+    eglDestroySurface(display, surface);
+    eglDestroyContext(display, context);
+    eglTerminate(display);
+
     return 0;
 
 }
@@ -270,12 +282,14 @@ void grab(int FBO)
 
     unsigned char *mpixels =new unsigned char[Width * Height * 4];//Width和Height为所要保存的屏幕图像的宽度与高度
     cout<< FBO <<endl;
-    glBindFramebuffer(GL_READ_FRAMEBUFFER, FBO);   
+    //glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);   
+    //eglGetCurrentSurface(EGL_READ);
+
     glReadPixels(viewPort[0], viewPort[1], viewPort[2], viewPort[3], GL_RGBA, GL_UNSIGNED_BYTE, mpixels);   
     //glBindBufferARB(GL_PIXEL_PACK_BUFFER_ARB, pboIds[0]);
 
     //glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
-    glBindFramebuffer(GL_READ_FRAMEBUFFER,0);                  
+    //glBindFramebuffer(GL_READ_FRAMEBUFFER,0);                  
     for(int i=0; i<(int)Width*Height*4; i+=4)
     { 
         //cout<< (int)mpixels[i] <<" "<< (int)mpixels[i+1] <<" "<<(int)mpixels[i+2]<<" "<<(int)mpixels[i+3] <<endl; 
